7330eb8c8b3eb944839a017d24e44ba38b367eda
ewnsong17 <ewnsong17@naver.com>
2023-01-12 17:55:14 +0900

    주석 수정

--- GameApp/GameApp.cpp
+++ GameApp/GameApp.cpp
@@ -1,20 +1,4 @@
 #include <Windows.h>
-//UINT = Unsigned Int
-//WPARAM = word-parameter (과거 16비트 -> 현재 32비트)
-//LPARAM = long-parameter (과거부터 32비트)
-//LRESULT = long, HRESULT = int
-//LPVOID = void*
-//HINSTANCE = InstanceID, Handle + Instance (not ProcessID)
-//LOWORD = int형 자료의 하위 2바이트 (HIWORD -> 상위 2바이트)
-//HINSTANCE = InstanceID, Handle + Instance (not ProcessID)
-
-//LP = Long Pointer, 64 or 32 포인터 값
-//C = Constant = const
-//W = Wide Char = Unicode
-//LPCSTR =  불변 문자 포인터 값
-//LPCWSTR = 유니코드 불변 문자 포인터 값
-
-//CALLBACK = 콜백 함수
 
 
 LRESULT CALLBACK wnd_proc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
